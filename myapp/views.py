968bc3209c328807ac7ec65ef1d4e7f9e5da8db8
Your Name <you@example.com>
2021-12-23 20:34:29 +0700

    2

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+import datetime
+
 
 def hello(request):
-   text = """<h1>welcome to my app !</h1>"""
-   return HttpResponse(text)
+   today = datetime.datetime.now().date()
+   return render(request, "hello.html", {"today" : today})
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import include, url
 
-from myapp.views import hello
+#from myapp.urls import myapp
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^hello/', hello, name = 'hello'),
+    url(r'^myapp/', include('myapp.urls')),
 
     # ... your url patterns
 ]
