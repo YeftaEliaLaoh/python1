bb329cb672b2557f14d0212272d11516b8433a5a
Your Name <you@example.com>
2021-12-23 21:48:09 +0700

    5

--- myapp/models.py
+++ myapp/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
+class Online(models.Model):
+
+	domain = models.CharField(max_length = 30)
+
+	class Meta:
+		db_table = "online"
+
 class Dreamreal(models.Model):
 
-   website = models.CharField(max_length = 50)
-   mail = models.CharField(max_length = 50)
-   name = models.CharField(max_length = 50)
-   phonenumber = models.IntegerField()
+	website = models.CharField(max_length = 50)
+	mail = models.CharField(max_length = 50)
+	name = models.CharField(max_length = 50)
+	phonenumber = models.IntegerField()
 
-   class Meta:
-      db_table = "dreamreal"
+	class Meta:
+		db_table = "dreamreal"
+
--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^hello/', views.hello),
+    url(r'^article/(\d+)/', views.viewArticle),
+    url(r'^articles/(\d{2})/(\d{4})', views.viewArticles),
 ]
